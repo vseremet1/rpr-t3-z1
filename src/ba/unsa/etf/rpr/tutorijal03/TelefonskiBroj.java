4995df5aa322a70111d17b9c173de56ddf49c95a
vseremet1 <vseremet1@etf.unsa.ba>
2018-11-02 17:34:27 +0100

    telefonski broj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public abstract class TelefonskiBroj {
+
+ public TelefonskiBroj ();
+ public abstract String Ispisi ();
+  public abstract int hashCode();
+
+}
